476a9a54c31fd4b8a9409308ad75c9799012ed8e
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-18 15:38:09 +0530

    feat(layout): add screenOptions to Stack component for header customization

--- Navigation/app/_layout.tsx
+++ Navigation/app/_layout.tsx
@@ -2,7 +2,18 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          // backgroundColor: "red",
+        },
+        // headerTintColor: "#fff",
+        headerTitleStyle: {
+          // fontWeight: "900",
+          // fontSize: 40,
+        },
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Main" }} />
       <Stack.Screen name="home" options={{ title: "Home" }} />
     </Stack>
