8806d6345929f5705bd98b5ae902431882dd572b
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-23 19:04:05 +0530

    feat(layout): update header styling and screen titles in RootLayout component

--- Navigation/app/_layout.tsx
+++ Navigation/app/_layout.tsx
@@ -1,21 +1,40 @@
+// import { Stack } from "expo-router";
+
+// export default function RootLayout() {
+//   return (
+//     <Stack
+//       screenOptions={{
+//         headerStyle: {
+//           // backgroundColor: "red",
+//         },
+//         // headerTintColor: "#fff",
+//         headerTitleStyle: {
+//           // fontWeight: "900",
+//           // fontSize: 40,
+//         },
+//       }}
+//     >
+//       <Stack.Screen name="index" options={{ title: "Main" }} />
+//       <Stack.Screen name="home" options={{ title: "Home" }} />
+//     </Stack>
+//   );
+// }
+
+import { View, Text } from "react-native";
+import React from "react";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
-        headerStyle: {
-          // backgroundColor: "red",
-        },
-        // headerTintColor: "#fff",
-        headerTitleStyle: {
-          // fontWeight: "900",
-          // fontSize: 40,
-        },
+        headerStyle: { backgroundColor: "lightblue" },
+        headerTintColor: "white",
+        headerTitleStyle: { fontWeight: "bold", fontSize: 30 },
       }}
     >
-      <Stack.Screen name="index" options={{ title: "Main" }} />
-      <Stack.Screen name="home" options={{ title: "Home" }} />
+      <Stack.Screen name="index" options={{ title: "MAIN PAGE" }} />
+      <Stack.Screen name="home" options={{ title: "HOME SCREEN" }} />
     </Stack>
   );
 }
